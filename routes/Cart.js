85a6954434ceddb0a7c299d3d644c8af300b98f2
licious <licious@LANCELOT.local>
2021-10-21 11:44:34 +0530

    Delete Functionality

--- routes/Cart.js
+++ routes/Cart.js
@@ -2,6 +2,10 @@ const express = require('express');
 const fetchUser = require('../middlewares/fetchuser');
 const User  = require('../models/User');
 const Cart = require("../models/Cart");
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+const JWT_AUTH_TOKEN = process.env.JWT_AUTH_TOKEN ;
 
 const router = express.Router();
 
@@ -13,6 +17,7 @@ router.get('/getCartItems' , fetchUser , async(req,res)=>{
 try{
     const userId  =  req.user.id ;
     const items = await Cart.find({user : userId});
+    
     res.status(200).json(items);
  }
  catch(error){
@@ -25,22 +30,24 @@ try{
 // update order count ;
 router.post('/updateOrder' , fetchUser , async(req, res) => {
 
-  //  console.log("in update order");
+ 
   try{
 
   const userId = req.user.id;
-  const userdetails = await User.findById(userId);
-  // console.log(userdetails);
 
   
-
+   if(userId){
     const cart =  await Cart.create({
       user : userId,
       name :req.body.name,
       amount:req.body.amount
     }); 
     // console.log(cart);
-    res.status(200).json({ success: true, message: "item added successfully" });
+   return res.status(200).json({ success: true, message: "item added successfully" });
+  }
+  else{
+    return res.status(401).json({success:false , message :  "Unvalid user"});
+    }
   }
   catch(err){
     console.log(err);
@@ -50,32 +57,55 @@ router.post('/updateOrder' , fetchUser , async(req, res) => {
   
 })
 
-router.put('/deleteItem/:id' , fetchUser , async(req,res) =>{
+router.put('/deleteItem'  , async(req,res) =>{
     try{
+
+     
+      let userId ;
+      let itemId = req.body.itemID ;
+      const authToken = req.header("authToken");
+    
+    if (!authToken) {
+      res.status(401).json({ sucess: false, message: "Unvalid user" });
+      return ;
+    }
+    try {
+      const data = await jwt.verify(authToken, JWT_AUTH_TOKEN);
+      // console.log(data);
+     userId= data.user.id;
       
-    const userId = req.user.id ;
+    } catch (error) {
+      console.log(error.message);
+      res.status(401).json({
+        sucess: false,
+        message: " Unvalid user with wrong credentails",
+      });
+    }
+
     // console.log(userId);
      if(userId){
-      let itemdetails = await Cart.findById(req.params.id);
-
+    
+     
+      let itemdetails = await Cart.findById(itemId);
+     
       if(!itemdetails){
         res.status(403).json({success : false , message: " Item not found"});
         return ;
       }
-
+   
       if(itemdetails && itemdetails.user.toString() !== userId){
         res.status(403).json({success:false , message : "Not allowed"});
         return;
       }
-
-      itemdetails  = await Cart.findByIdAndDelete(req.params.id);
-
+      
+      itemdetails  = await Cart.findByIdAndDelete(itemId);
+ 
        res.status(200).json({success:true  , message:"Item deleted"});
     }
     }
     catch(err){
       
-      console.log(err);
+      console.log(err.message);
     }
 })
 
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const cookieParser = require("cookie-parser");
 
  // Your Auth Token from www.twilio.com/console
-app.use(cors({ origin: "http://localhost:3000/" , credentials : true}));
+app.use(cors({ origin: "*"}));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -17,11 +17,7 @@ app.use('/api/auth',require('./routes/authOTP'));
 app.use('/api/Cart' , require('./routes/Cart'));
 app.use('/api/update' , require('./routes/userUpdate'));
 
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
-    res.header("Access-Control-Allow-Headers" , "Origin,X-Requested-With , Content-Type ,Accept");
-    next();
-});
+
     
  connectTomongo();
 app.get('/',(req,res)=>{
